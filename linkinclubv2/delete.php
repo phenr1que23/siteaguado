9876928dbc0f0ca603c238d72771dbc3f036e893
phenr1que23 <p3dro.henr7q23@gmail.com>
2024-12-04 21:37:16 -0300

    Add files via upload

---
+++ linkinclubv2/delete.php
@@ -0,0 +1,16 @@
+/*create database loja;
+use loja;
+
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(1000) NOT NULL,
+    descricao TEXT NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    imagem_url VARCHAR(255),
+    categoria VARCHAR(100),
+    estoque INT
+);
+
+insert produtos values(1, "GUITARRA", "VIDE FICHE", "20.5", "http://localhost/linkinclubv2/img/meteora.jpg", "Instrumento", 2 )
+insert produtos values(12, "GUITARRA", "VIDE FICHE", "20.5", "http://localhost/linkinclubv2/img/meteora.jpg", "Instrumento", 2 ) */
